9c143cc9d51393211afee647b67b804db460bcd3
hometownzero <bradycpeters@gmail.com>
2018-03-18 03:11:24 -0500

    added some basic get/post endpoints with a fake store, handling json properly

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder="public", template_folder="public")
@@ -8,11 +8,49 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-#from models import User
+posts = [
+    {
+        'id': 1,
+        'title': 'first post',
+        'body': 'first body'
+    },
+    {
+        'id': 2,
+        'title': 'second post',
+        'body': 'second body'
+    },
+    {
+        'id': 3,
+        'title': 'third post',
+        'body': 'third body'
+    },
+]
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/post', methods=['POST'])
+def create_post():
+    request_data = request.get_json()
+    new_post = {
+        'id': 4,
+        'title': request_data['title'],
+        'body' : request_data['body']
+    }
+    posts.append(new_post)
+    return jsonify(new_post)
+
+@app.route('/post', methods=['GET'])
+def get_posts():
+    return jsonify({'posts': posts})
+
+@app.route('/post/<int:id>')
+def get_post_by_id(id):
+    for post in posts:
+        if post['id'] == id:
+            return jsonify(post)
+    return jsonify({'message': 'post not found'})
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,16 +1,16 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
 
-class User(db.Model):
-    __tablename__ = 'users'
+class Post(db.Model):
+    __tablename__ = 'posts'
 
     id       = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String())
-    password = db.Column(db.String())
+    title = db.Column(db.String())
+    body = db.Column(db.String())
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
