6b66a4c96c92c81fb7f986f5d3ea1e0b4a53d7c5
hometownzero <bradycpeters@gmail.com>
2018-03-19 21:45:04 -0500

    connected to postgres db, created endpoints for adding resource and getting all resources.

--- app.py
+++ app.py
@@ -1,56 +1,81 @@
-import sys, os
+import sys, os, json
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from flask import Flask, render_template, jsonify, request
+from flask_restful import Resource, Api, reqparse
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder="public", template_folder="public")
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+api = Api(app)
 
-posts = [
-    {
-        'id': 1,
-        'title': 'first post',
-        'body': 'first body'
-    },
-    {
-        'id': 2,
-        'title': 'second post',
-        'body': 'second body'
-    },
-    {
-        'id': 3,
-        'title': 'third post',
-        'body': 'third body'
-    },
-]
+from models import *
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route('/post', methods=['POST'])
-def create_post():
-    request_data = request.get_json()
-    new_post = {
-        'id': 4,
-        'title': request_data['title'],
-        'body' : request_data['body']
-    }
-    posts.append(new_post)
-    return jsonify(new_post)
+class Post(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('title',
+        type=str,
+        required=True,
+        help="this field cant be blank"
+    )
+    parser.add_argument('body',
+        type=str,
+        required=True,
+        help="this field cant be blank"
+    )
 
-@app.route('/post', methods=['GET'])
-def get_posts():
-    return jsonify({'posts': posts})
+    def get(self, title):
+        post = next(filter(lambda x: x['title'] == title, posts), None)
+        return {'post': post}, 200 if post else 404
 
-@app.route('/post/<int:id>')
-def get_post_by_id(id):
-    for post in posts:
-        if post['id'] == id:
-            return jsonify(post)
-    return jsonify({'message': 'post not found'})
+    def post(self, title):
+
+        data = Post.parser.parse_args()
+        title = data['title']
+        body = data['body']
+        new_post = BlogPost(title, body)
+        
+        post_add=new_post.add(new_post)
+        if post_add:
+            return {'title': new_post.title, 'body': new_post.body}, 200
+        else:
+            return {'error': 'did not work'}, 500
+
+    def delete(self, title):
+        global posts
+        posts = list(filter(lambda x: x['title'] != title, posts))
+        return {'success':'post deleted'}
+
+    def put(self, title):
+        data = Post.parser.parse_args()
+        post = next(filter(lambda x: x['title'] == title, posts), None)
+        if post is None:
+            return {'error': 'post not found'}, 404
+        else:
+            post.update(data)
+            return post
+
+class PostList(Resource):
+    def get(self):
+        posts = BlogPost.query.all()
+        dict_posts = []
+        for post in posts:
+            dict = {
+                'id': post.id,
+                'title': post.title,
+                'body':post.body
+            }
+            dict_posts.append(dict)
+        print(dict_posts)
+        return dict_posts
+
+api.add_resource(Post, '/post/<string:title>')
+api.add_resource(PostList, '/posts')
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,16 +1,28 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
 
-class Post(db.Model):
+class BlogPost(db.Model):
     __tablename__ = 'posts'
 
-    id       = db.Column(db.Integer, primary_key=True)
+    id    = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String())
-    body = db.Column(db.String())
+    body  = db.Column(db.String())
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
 
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
+    def add(self, post):
+        db.session.add(post)
+        db.session.commit()
+        return True
+
+    def update(self):
+        return session_commit()
+    
+    def delete(self, post):
+        db.session.delete(post)
+        return session_commit
+
+    #def __repr__(self):
+    #    return '<id {}>'.format(self.id)
